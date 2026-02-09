7834048ceba89d431d63a7a226bdc660334ea009
Hamza El Rachdi <hamzarachidi04@gmail.com>
2026-02-09 20:50:08 +0200

    feat: enhance metadata handling and improve layout; update build and start scripts for standalone output

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import LandingPage from "@/modules/Landing-Page/LandingPage";
+
+export const metadata: Metadata = {
+  title: "Welcome",
+  description: "The central hub for all student clubs and activities at AUB.",
+};
 
 export default function Home() {
   return (
