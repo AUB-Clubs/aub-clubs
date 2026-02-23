9cb179c573949f9930737b9ef334ac78bc00215a
Hamza El Rachdi <hamzarachidi04@gmail.com>
2026-02-24 01:45:48 +0200

    Remove `createUser` procedure, `zod` import, and `baseProcedure` from the app router.

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,26 +1,14 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { createTRPCRouter } from '../init';
 import { profileRouter } from '../../modules/Students/server/profile';
 import { forYouRouter } from '../../modules/Students/server/forYou';
 import { clubsRouter } from '../../modules/Clubs/server/clubs';
 import { commitmentLevelRouter } from '../../modules/Clubs/server/commitmentLevel';
 
 export const appRouter = createTRPCRouter({
-  profile:         profileRouter,
-  forYou:          forYouRouter,
-  clubs:           clubsRouter,
+  profile: profileRouter,
+  forYou: forYouRouter,
+  clubs: clubsRouter,
   commitmentLevel: commitmentLevelRouter,
-  createUser: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      }),
-    )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
 });
 
 export type AppRouter = typeof appRouter;
